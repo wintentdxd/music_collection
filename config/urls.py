b3fd762df31c82a2ee4010204b01bbb07a17f4b5
wintentdxd <dariy.gulyk@gmail.com>
2026-09-25 15:52:14 +0300

    Виконати домашнє завдання №3: CBV, CRUD-операції, пагінація та шаблони форм.

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,11 @@ from music import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.album_list, name='album_list'),
-    path('albums/<int:pk>/', views.album_detail, name='album_detail'),
-    path('artists/', views.artist_list, name='artist_list'),
+    path('', views.AlbumListView.as_view(), name='album_list'),
+    path('top-albums/', views.TopAlbumsView.as_view(), name='top_albums'),
+    path('albums/new/', views.AlbumCreateView.as_view(), name='album_create'),
+    path('albums/<int:pk>/', views.AlbumDetailView.as_view(), name='album_detail'),
+    path('albums/<int:pk>/edit/', views.AlbumUpdateView.as_view(), name='album_update'),
+    path('albums/<int:pk>/delete/', views.AlbumDeleteView.as_view(), name='album_delete'),
+    path('artists/', views.ArtistListView.as_view(), name='artist_list'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,17 +1,75 @@
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
 from .models import Album, Artist
 
-# 1. Список альбомів (Головна сторінка)
-def album_list(request):
-    albums = Album.objects.all()
-    return render(request, 'music/album_list.html', {'albums': albums})
+# ЧАСТИНА 1: CBV ДЛЯ ПЕРЕГЛЯДУ
 
-# 2. Сторінка деталей альбому (з get_object_or_404)
-def album_detail(request, pk):
-    album = get_object_or_404(Album, pk=pk)
-    return render(request, 'music/album_detail.html', {'album': album})
+# 1. Список альбомів з пагінацією
+class AlbumListView(ListView):
+    model = Album
+    template_name = 'music/album_list.html'
+    context_object_name = 'albums'
+    ordering = ['title']
+    paginate_by = 5
 
-# 3. Друга сторінка-список (Виконавці)
-def artist_list(request):
-    artists = Artist.objects.all()
-    return render(request, 'music/artist_list.html', {'artists': artists})
+
+# 2. Другий список із фільтрацією
+class TopAlbumsView(ListView):
+    model = Album
+    template_name = 'music/album_list.html'
+    context_object_name = 'albums'
+    paginate_by = 5
+
+    def get_queryset(self):
+        return Album.objects.filter(rating__gte=4).order_by('-rating')
+
+
+# 3. Деталі альбому з додатковим контекстом
+class AlbumDetailView(DetailView):
+    model = Album
+    template_name = 'music/album_detail.html'
+    context_object_name = 'album'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['other_albums'] = Album.objects.filter(
+            artist=self.object.artist
+        ).exclude(pk=self.object.pk)
+        return context
+
+
+# 4. Список виконавців
+class ArtistListView(ListView):
+    model = Artist
+    template_name = 'music/artist_list.html'
+    context_object_name = 'artists'
+
+
+# ЧАСТИНА 2: CRUD ОПЕРАЦІЇ
+class AlbumCreateView(CreateView):
+    model = Album
+    fields = ['title', 'artist', 'genres', 'album_type', 'release_date', 'rating']
+    template_name = 'music/album_form.html'
+
+
+class AlbumUpdateView(UpdateView):
+    model = Album
+    fields = ['title', 'artist', 'genres', 'album_type', 'release_date', 'rating']
+    template_name = 'music/album_form.html'
+
+
+class AlbumDeleteView(DeleteView):
+    model = Album
+    template_name = 'music/album_confirm_delete.html'
+    success_url = reverse_lazy('album_list')
+
+
+# def album_list(request):
+#     ...
+# def album_detail(request, pk):
+#     ...
+# def artist_list(request):
+#     ...
